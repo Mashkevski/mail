fcc9e508902c5781d3cab87ba5c3c5b6f1892a2b
Siarhei Mashkeuski <cybera@list.ru>
2019-04-21 15:09:20 +0300

    style: formatting code

--- source/js/script.js
+++ source/js/script.js
@@ -5,11 +5,11 @@ const mainNavToggle = mainHeader.querySelector('.main-nav-toggle');
 mainHeader.classList.remove('menu-nojs');
 
 mainNavToggle.addEventListener('click', function () {
-  if (mainHeader.classList.contains('menu-closed')) {
-    mainHeader.classList.remove('menu-closed');
-    mainHeader.classList.add('menu-opened');
-  } else {
-    mainHeader.classList.add('menu-closed');
-    mainHeader.classList.remove('menu-opened');
-  }
+    if (mainHeader.classList.contains('menu-closed')) {
+        mainHeader.classList.remove('menu-closed');
+        mainHeader.classList.add('menu-opened');
+    } else {
+        mainHeader.classList.add('menu-closed');
+        mainHeader.classList.remove('menu-opened');
+    }
 });
